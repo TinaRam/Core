3d258ad8ac3c1fcf11862b9f3da06636acc74b45
TinaHodepina <31168143+TinaRam@users.noreply.github.com>
2019-04-11 19:37:35 +0200

    Added migration "UserImageBlob"

--- Workflow/Migrations/20190411173146_UserImageBLOB.Designer.cs
+++ Workflow/Migrations/20190411173146_UserImageBLOB.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Workflow.Models;
 
 namespace Workflow.Migrations
 {
     [DbContext(typeof(WorkflowContext))]
-    partial class WorkflowContextModelSnapshot : ModelSnapshot
+    [Migration("20190411173146_UserImageBLOB")]
+    partial class UserImageBLOB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
